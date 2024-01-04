2a046383ed9680f441e30169679c938d01db8824
rakshith <rakshit.s.gowda@gmail.com>
2024-01-04 08:33:27 +0530

    skills

---
+++ constants/skills.jsx
@@ -0,0 +1,7 @@
+export const skills = [
+  { name: 'Git', src: '/assets/Git.png', alt: 'git' },
+  { name: 'Git', src: '/assets/Git.png', alt: 'git' },
+  { name: 'Git', src: '/assets/Git.png', alt: 'git' },
+  { name: 'Git', src: '/assets/Git.png', alt: 'git' },
+  { name: 'Git', src: '/assets/Git.png', alt: 'git' },
+]
